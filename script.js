d453951f0c1e6809a9c0f0c1f9edecbf9cd30498
thebestmoversnationwide <thebestmoversfl@gmail.com>
2026-09-09 15:57:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const SUPABASE_KEY = 'sb_publishable_q6vkNsHyMqU0uXfDSisV4g_6v_iGmWG';
 document.getElementById('year').textContent = new Date().getFullYear();
 
 document.getElementById('quickQuote').addEventListener('submit', function(e) {
